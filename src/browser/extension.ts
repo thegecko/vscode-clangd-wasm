1c51eb544d2e4e15bcc06e9acf5e94f7bcc8153f
robmor01 <Rob.Moran@arm.com>
2024-10-30 16:37:34 +0000

    Initial commit

---
+++ src/browser/extension.ts
@@ -0,0 +1,4 @@
+import * as vscode from 'vscode';
+
+export const activate = async (context: vscode.ExtensionContext): Promise<void> => {
+};
---
+++ src/manifest.ts
@@ -0,0 +1,4 @@
+export const PACKAGE_NAME = 'clangd-wasm';
+export const DISPLAY_NAME = 'clangd WASM';
+export const CONFIG_LOGGING_VERBOSITY = 'loggingVerbosity';
+export const DEFAULT_LOGGING_VERBOSITY = 'warn';
